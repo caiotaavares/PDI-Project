4f3ba41bc336c4ab172e4434acab7f974bb5a2dd
caiotaavares <caio_s.tavares2@hotmail.com>
2022-05-17 23:07:11 -0300

    Primary Interface

---
+++ unit1.pas
@@ -0,0 +1,218 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus,
+  StdCtrls, Windows;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
+    MenuItem11: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    N1: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
+    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
+      );
+    procedure MenuItem10Click(Sender: TObject);
+    procedure MenuItem11Click(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem4Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
+    procedure MenuItem6Click(Sender: TObject);
+    procedure MenuItem7Click(Sender: TObject);
+    procedure MenuItem8Click(Sender: TObject);
+    procedure MenuItem9Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  imE, imS : array [0..319,0..239] of byte;
+  r, g, b : byte;
+  cor : TColor;
+  c, i, j, x, y, alt, larg, Qtde : integer;
+
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.MenuItem5Click(Sender: TObject);
+begin
+  Close();
+end;
+
+procedure TForm1.MenuItem6Click(Sender: TObject);
+begin
+// Implementar os filtros da média e da mediana
+   for j := 0 to Image1.Width - 1 do
+       begin
+       for i := 0 to Image1.height - 1 do
+           begin
+           Ims[i,j] := Ime[i,j];
+           Image2.Canvas.Pixels[i,j] := RGB(Ims[i,j],Ims[i,j],Ims[i,j]);
+           end;
+       end;
+alt := Image1.height;
+larg := Image1.Width;
+Qtde := (alt + larg)/10;
+for i := 0 to Qtde do
+    begin
+    x := random(alt);
+    y := random(larg);
+    // ou 0 ou 255
+    cor := 255;
+    Ims[x,y] := cor;
+    Image2.Canvas.Pixels[i,j] := RGB(Ims[x,y], Ims[x,y], Ims[x,y]);
+    end;
+end;
+
+procedure TForm1.MenuItem7Click(Sender: TObject);
+begin
+  for i:= 0 to Image1.Width - 1 do
+   for j:= 0 to Image1.height - 1 do
+    begin
+      cor := Image1.Canvas.Pixels[i,j];
+      r := GetRValue(cor);
+      g := GetGValue(cor);
+      b := GetBValue(cor);
+      c := (r + g + b) div 3;
+      Ims[i,j] := c;
+      Image2.Canvas.Pixels[i,j] := RGB(c,c,c);
+    end;
+end;
+
+procedure TForm1.MenuItem8Click(Sender: TObject);
+begin
+
+  for i:= 0 to Image1.Width - 1 do
+   for j:= 0 to Image1.height - 1 do
+    begin
+      Ims[i,j] := 255 - Ime[i,j];
+      Image2.Canvas.Pixels[i,j] := RGB(Ims[i,j],Ims[i,j],Ims[i,j]);
+    end;
+end;
+
+procedure TForm1.MenuItem9Click(Sender: TObject);
+begin
+  for i:= 0 to Image1.Width - 1 do
+   for j:= 0 to Image1.height - 1 do
+    begin
+      cor := Image1.Canvas.Pixels[i,j];
+      r := GetRValue(cor);
+      g := GetGValue(cor);
+      b := GetBValue(cor);
+      Image2.Canvas.Pixels[i,j] := RGB(255-r,255-g,255-b);
+    end;
+end;
+
+procedure TForm1.MenuItem3Click(Sender: TObject);
+begin
+  if (OpenDialog1.Execute)
+   then
+    Image1.Picture.LoadFromFile(OpenDialog1.FileName);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Image1.Picture := Image2.Picture;
+
+  for i:= 0 to Image1.Width - 1 do
+   for j:= 0 to Image1.height - 1 do
+    Ime[i,j] := Ims[i,j];
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Image1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+  Edit1.Text := IntToStr(X);
+  Edit2.Text := IntToStr(Y);
+  cor := Image1.Canvas.Pixels[X,Y];
+
+  for i:= 0 to Image5.Width - 1 do
+   for j:= 0 to Image5.height - 1 do
+    Image5.Canvas.Pixels[i,j] := cor;
+
+end;
+
+procedure TForm1.MenuItem10Click(Sender: TObject);
+begin
+  for i:= 0 to Image1.Width - 1 do
+   for j:= 0 to Image1.height - 1 do
+    begin
+      cor := Image1.Canvas.Pixels[i,j];
+      r := GetRValue(cor);
+      g := GetGValue(cor);
+      b := GetBValue(cor);
+
+      Image2.Canvas.Pixels[i,j] := RGB(r,0,0);   // vermelho
+      Image3.Canvas.Pixels[i,j] := RGB(0,g,0);   // verde
+      Image4.Canvas.Pixels[i,j] := RGB(0,0,b);   // azul
+    end;
+end;
+
+procedure TForm1.MenuItem11Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.MenuItem4Click(Sender: TObject);
+begin
+  if (SaveDialog1.Execute)
+   then
+    Image2.Picture.SaveToFile(SaveDialog1.FileName);
+end;
+
+end.
+
